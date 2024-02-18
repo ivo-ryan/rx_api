3dc9066a73b9ebb577a0520dd1e5582107bf027e
Ivo Ryan <ryanbussines06@gmail.com>
2024-02-17 22:41:51 -0300

    Api pronta para o deploy

---
+++ src/index.js
@@ -0,0 +1,115 @@
+
+const express = require ("express");
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+const User = mongoose.model('User', {
+
+    email: String || Number,
+    senha: String ,
+})
+
+const Animes = mongoose.model('Animes', {
+    name: String,
+    description: String,
+    image_url: String,
+    epsodios: [] ,
+});
+
+const Filmes = mongoose.model('Filmes' , {
+    name: String,
+    description: String,
+    image_url: String,
+    filme_url: String,
+})
+
+app.get("/" , async (req , res) => {
+    const animes = await Animes.find();
+   return  res.send( animes )
+})
+
+app.get("/filmes", async (req , res) => {
+    const filmes = await Filmes.find();
+    return res.send( filmes )
+});
+
+app.put("/filmes/:id", async ( req, res ) => {
+    const filme = await Filmes.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        image_url: req.body.image_url,
+        description: req.body.description,
+        filme_url: req.body.filme_url,
+    })
+
+    return res.send(filme)
+})
+
+app.post("/user", async (req , res) => {
+        const user = new User({
+            email: req.body.email,
+            senha: req.body.senha,
+        })
+
+       await user.save()
+       return res.send(user)
+})
+
+app.get("/user" , async (req , res) => {
+    const user = await User.find()
+   return  res.send(user)
+})
+
+app.delete('/user/:id', async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    return res.send(user)
+})
+
+app.post("/animes", async (req , res) => {
+    const animes = new Animes({
+        name: req.body.name,
+        image_url: req.body.image_url,
+        description: req.body.description,
+        epsodios: req.body.epsodios,
+    })
+
+    await animes.save()
+    return res.send(animes)
+});
+
+app.put("/animes/:id", async (req, res) => {
+    const anime = await Animes.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        image_url: req.body.image_url,
+        description: req.body.description,
+        epsodios: req.body.epsodios
+    })
+
+    return res.send(anime)
+})
+
+
+app.post("/filmes", async (req , res) => {
+    const filme = new Filmes({
+        name: req.body.name,
+        image_url: req.body.image_url,
+        description: req.body.description,
+        filme_url: req.body.filme_url,
+    })
+
+    await filme.save()
+    return res.send(filme)
+});
+
+app.listen(port, async () => {
+    await mongoose.connect('mongodb+srv://ryan3000:AnimesRX2.0@animesrx.ko9cf6y.mongodb.net/?retryWrites=true&w=majority');
+    console.log("App running");
+});
+
+
