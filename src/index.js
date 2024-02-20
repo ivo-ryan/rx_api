fdfdcccc150f40b067d33e173d188b33ce693642
Ivo Ryan <ryanbussines06@gmail.com>
2024-02-19 23:41:43 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 
 const express = require ("express");
 const mongoose = require('mongoose');
+const  cors = require('cors')
 
 
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
+})
 
 const port = process.env.PORT || 3000;
 
