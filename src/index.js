5f179b50ba1bf0df6df1bebd3346d3650d51f9f7
Ivo Ryan <ryanbussines06@gmail.com>
2024-03-05 23:11:42 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header ("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
 
     app.use(cors());
     next();
